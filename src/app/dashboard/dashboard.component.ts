9af4a8853a28b35ffe202eef4d1ca3a50d7ea3ec
yyaroshevich <yyaroshevich@exadel.com>
2017-08-24 21:51:29 +0300

    The Hero Editor: Finishing Routing, styles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {Hero} from '../models/hero';
 import {HeroService} from '../shared/hero.service';
 
 @Component({
   selector: 'dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService,
+              private router: Router) {}
 
   ngOnInit(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  goToDetail(id: number): void {
+    this.router.navigate(['/hero', id]);
   }
 }
